185950d164fc474e132c558ef942126975dd0cce
Divyansh Bhushan <divyanshbhushan.business@gmail.com>
2024-02-18 16:50:54 +0530

    Commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,15 @@
+const addNewExpense = () => {
+  const addNewExpenseContainer = document.querySelector("#addNewExpenseContainer");
+  const addNewExpenseTogglers = document.querySelectorAll(".addNewExpenseToggler");
+  addNewExpenseTogglers.forEach((addNewExpenseToggler)=>{
+    addNewExpenseToggler.addEventListener("click", () => {
+      if(addNewExpenseContainer.style.scale === "1"){
+        addNewExpenseContainer.style.scale = 0;
+      } else{
+        addNewExpenseContainer.classList.remove('scale-[0]')
+        addNewExpenseContainer.style.scale = 1;
+      }
+    })
+  })
+};
+addNewExpense();
